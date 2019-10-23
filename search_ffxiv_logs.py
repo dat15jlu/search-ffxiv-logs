a1d00b126431ce04036560f6864a963f71a21e01
Jonas <dat15jlu@student.lu.se>
2019-10-23 14:34:29 +0200

    Initial commit

---
+++ search_ffxiv_logs.py
@@ -0,0 +1,65 @@
+from os import listdir
+import regex as re
+import sys
+
+
+def main():
+    if len(sys.argv) > 2:
+        files = [sys.argv[1] + "/" + file for file in listdir(sys.argv[1])]
+        regex = sys.argv[2]
+        all_matches = find_all_matches(files, regex)
+        print_matches(all_matches)
+    else:
+        print()
+        print("Not enough input parameters. Expected 2, got " + str(len(sys.argv) - 1) + ".")
+        print("First parameter: directory with the log files. For example: \"log\".")
+        print("Second parameter: text to search. For example: \"Annie Keito\".")
+        print("You can run this program in PowerShell like this:")
+        print()
+        print(".\\search_ffxiv_logs.exe log \"Annie Keito\" > output.txt")
+        print()
+        print("If you are running it like this, make sure the log directory is")
+        print("in the same directory as this .exe file.")
+        input("Press ENTER to close.")  # to keep CMD window open
+
+
+def print_matches(all_matches):
+    for key in all_matches.keys():
+        if len(all_matches[key]) > 0:
+            print("#################### " + key + ":\n")
+            for match in all_matches[key]:
+                print("\t" + match)
+
+
+def find_all_matches(files, regex):
+    all_matches = {}
+    for file in files:
+        matches_in_file = []
+        lines = read_file_to_string(file)
+        for line in lines:
+            if regex in line:
+                matches_in_file.append(line)
+        all_matches[file] = matches_in_file
+    return all_matches
+
+
+def read_file_to_string(file):
+    interesting_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\n()[],.?!:' "
+    f = open(file, encoding="ISO-8859-1")
+    contents = ""
+    while True:
+        try:
+            c = f.read(1)
+        except UnicodeDecodeError:
+            c = ' '
+        if not c:  # EOF
+            break
+        if c in interesting_chars:
+            contents += c
+        else:
+            contents += ' '
+    contents = re.sub(' +', ' ', contents)
+    return contents.split('\n')
+
+
+main()
